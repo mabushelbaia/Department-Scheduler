2b8acf96091c3782c5fdc9202dc1789c9a9b8e15
mabushelbaia <mabushelbaia@gmail.com>
2023-01-13 08:03:27 +0200

    Final Version

--- Course.py
+++ Course.py
@@ -1,29 +1,34 @@
 import prettytable
-from random import random
+import random
 from typing import *
+import numpy as np
 
 
 Gene = list
 Schedule = list[Gene]
+
+
 class Course:
     def __init__(self, attributes, teachers):
         self.code = attributes[0].strip()
         self.name = attributes[1]
         self.sections = int(attributes[2].strip("\n"))
         self.credit = int(attributes[0][5])
-        self.year   = int(attributes[0][4])
+        self.year = int(attributes[0][4])
         self.type = "Lecture" if self.credit == 3 else "Lab"
-        teachers = teachers
+        self.teachers = teachers
+
     def __str__(self):
         return " ".join([self.code, self.name, str(self.sections)])
 
-lecture_slots = [("M", "W", "08:30 - 9:45"),("S", "W", "08:30 - 09:45"), ("S", "M", "08:30 - 09:45"), ("T", "R", "08:30 - 09:45"),
-                ("M", "W", "10:00 - 11:15"),("S", "W", "10:00 - 11:15"), ("S", "M", "10:00 - 11:15"), ("T", "R", "10:00 - 11:15"),
-                ("M", "W", "11:25 - 12:40"),("S", "W", "11:25 - 12:40"), ("S", "M", "11:25 - 12:40"), ("T", "R", "11:25 - 12:40"),
-                ("M", "W", "12:50 - 14:05"),("S", "W", "12:50 - 14:05"), ("S", "M", "12:50 - 14:05"), ("T", "R", "12:50 - 14:05")]
-lab_slots = [("S", "08:30 - 11:10"), ("M", "08:30 - 11:10"), ("T", "08:30 - 11:10"), ("W", "8:30 - 11:10"),("R", "08:30 - 11:10"),
-             ("S", "11:25 - 14:05"), ("M", "11:25 - 14:05"), ("T", "11:25 - 14:05"), ("W", "11:25 - 14:05"),("R", "11:25 - 14:05"),
-             ("S", "14:15 - 16:55"), ("M", "14:15 - 16:55"), ("T", "14:15 - 16:55"), ("W", "14:15 - 16:55"),("R", "14:15 - 16:55")]
+
+lecture_slots = [("M", "W", "08:30 - 09:45"), ("S", "W", "08:30 - 09:45"), ("S", "M", "08:30 - 09:45"), ("T", "R", "08:30 - 09:45"),
+                 ("M", "W", "10:00 - 11:15"), ("S", "W", "10:00 - 11:15"), ("S", "M", "10:00 - 11:15"), ("T", "R", "10:00 - 11:15"),
+                 ("M", "W", "11:25 - 12:40"), ("S", "W", "11:25 - 12:40"), ("S", "M", "11:25 - 12:40"), ("T", "R", "11:25 - 12:40"),
+                 ("M", "W", "12:50 - 14:05"), ("S", "W", "12:50 - 14:05"), ("S", "M", "12:50 - 14:05"), ("T", "R", "12:50 - 14:05")]
+lab_slots = [("S", "08:30 - 11:10"), ("M", "08:30 - 11:10"), ("T", "08:30 - 11:10"), ("W", "08:30 - 11:10"), ("R", "08:30 - 11:10"),
+             ("S", "11:25 - 14:05"), ("M", "11:25 - 14:05"), ("T", "11:25 - 14:05"), ("W", "11:25 - 14:05"), ("R", "11:25 - 14:05"),
+             ("S", "14:15 - 16:55"), ("M", "14:15 - 16:55"), ("T", "14:15 - 16:55"), ("W", "14:15 - 16:55"), ("R", "14:15 - 16:55")]
 courses = []
 with open("data.txt", "r") as f:
     data = f.readlines()
@@ -32,103 +37,121 @@ with open("data.txt", "r") as f:
         line[3] = line[3].strip("\n")
         res = eval(line[3])
         courses.append(Course(line[:3], res))
+
+
+def generate_chromosome() -> Schedule:
+    schedule = []
+    for course in courses:
+        gene = [course.year, course.type]
+        if course.type == "Lecture":
+            sections = random.choices(lecture_slots, k=course.sections)
+        else:
+            sections = random.sample(lab_slots, k=course.sections)
+        l = []
+        for key, value in course.teachers.items():
+            l.extend([key] * value)
+        sections = list(map(list, zip(l, sections)))
+        gene.append(sections)
+        schedule.append(gene)
+    return schedule
+
+
 def print_schedule(schedule: Schedule) -> None:
+    schedule = schedule.courses
     pt = prettytable.PrettyTable()
     # add lines between rows
     pt.hrules = prettytable.ALL
     pt.add_autoindex = True
     pt.set_style(prettytable.DOUBLE_BORDER)
-    pt.field_names = ["Course Code", "Course Name", "Sections", "Year", "Type", "Slots"]
+    pt.field_names = ["Course Code", "Course Name",
+                      "Sections", "Year", "Type", "Slots"]
     for i, course in enumerate(courses):
-        slots = "\n".join([str(slot) for slot in sorted(schedule[i][2], key=lambda x: x[1][0])])
-        pt.add_row([course.code, course.name, course.sections, course.year, course.type, slots])
+        slots = "\n".join([str(slot) for slot in sorted(
+            schedule[i][2], key=lambda x: x[1][0])])
+        pt.add_row([course.code, course.name, course.sections,
+                   course.year, course.type, slots])
     pt.sortby = "Year"
     print(pt)
+
 
 class Chromosome:
     def __init__(self, courses_list: Schedule) -> None:
         # generate a new chromomse if no courses_list is given
         if courses_list is None:
-            self.courses = self.generate_chromosome(courses)
+            self.courses = generate_chromosome()
         else:
-            courses = courses_list
-        self.attributes = self.calc(courses)
-        self.fitness = self.attributes[0]
-        self.teacher_slots = self.attributes[1]     
-        self.conflict_sum = self.attributes[2][0]
-        self.early_penalty = self.attributes[2][2]
-        self.late_penalty = self.attributes[2][3]
-        self.saturday_penalty = self.attributes[2][4]
-        self.sequenctial_pelanty = self.attributes[2][1]
-        print(self.fitness)
+            self.courses = courses_list
+        self.attributes = self.calc(self.courses)
+        if self.attributes[0]:
+            self.fitness = round(self.attributes[0], 4)
+            self.teacher_slots = self.attributes[1]
+            self.conflict_sum = self.attributes[2][0]
+            self.early_penalty = self.attributes[2][2]
+            self.late_penalty = self.attributes[2][3]
+            self.saturday_penalty = self.attributes[2][4]
+            self.sequenctial_pelanty = self.attributes[2][1]
+        else:
+            self.fitness = None
 
-    def generate_chromosome(self,courses: list[Course]) -> Schedule:
-        schedule = []
-        for course in courses:
-            gene = [course.year, course.type]
-            if course.type == "Lecture":
-                sections = random.choices(lecture_slots, k=course.sections)
-            else:
-                sections = random.sample(lab_slots, k=course.sections)
-            l = []
-            for key, value in course.teachers.items():
-                l.extend([key] * value)
-            sections = list(map(list, zip(l, sections)))
-            gene.append(sections)
-            schedule.append(gene)
-        return schedule
-
-    def calc(self,schedule: Schedule) -> Tuple[float, dict, list[int]]:
+    def calc(self, schedule: Schedule) -> Tuple[float, dict, list[int]]:
         fitness = 0
         teacher_slots = {}
-        unique_slots = set()          
+        lab_days = {}
+        unique_slots = set()
         early_penalty = 0
         late_penalty = 0
         saturday_penalty = 0
         sequenctial_pelanty = 0
         conflicts = {2: {}, 3: {}, 4: {}, 5: {}}
         for course in schedule:
-            local_lab_slots = {} # For checking lab conflicts
-            local_days = {} 
-            local_slots = set()
+            local_lab_slots = {}  # For checking lab conflicts
+            days_set = set()
+            days_count = {}
+            slots_set = set()
             for (teacher, slot) in course[2]:
                 if teacher in teacher_slots:
                     if slot in teacher_slots[teacher]:
-                        return 0, 0, 0
+                        return None, 0, 0
                     else:
                         teacher_slots[teacher].append(slot)
                 else:
                     teacher_slots[teacher] = [slot]
                 if course[1] == "Lab":
                     if slot in local_lab_slots:
-                        return 0, 0, 0
+                        return None, 0, 0
                     else:
                         local_lab_slots[slot] = True
                 if course[1] == "Lab":
-                    local_days[slot[0]] = local_days.get(slot[0], 0) + 1
+                    days_set.add(slot[0])
+                    slots_set.add(slot[1])
+                    lab_days[slot[0]] = lab_days.get(slot[0], 0) + 1
                 elif course[1] == "Lecture":
-                    local_days[slot[0]] = local_days.get(slot[0], 0) + 1
-                    local_days[slot[1]] = local_days.get(slot[1], 0) + 1
+                    days_count[(slot[0],slot[1])] = days_count.get((slot[0],slot[1]), 0) + 1
+                    days_set.add(slot[0])
+                    days_set.add(slot[0])
+                    slots_set.add(slot[2])
                     for i in range(len(course[2])):
                         for j in range(i+1, len(course[2])):
-                            if abs(lecture_slots.index(course[2][i][1]) - lecture_slots.index(course[2][j][1]))%len(lecture_slots) == 4: # 2 Slots together
+                            if abs(lecture_slots.index(course[2][i][1]) - lecture_slots.index(course[2][j][1])) % len(lecture_slots) == 4:
                                 sequenctial_pelanty += 1
                 if slot[-1] in ["08:30 - 9:45", "08:30 - 11:10"]:
-                        early_penalty += 1
+                    early_penalty += 1
                 if slot[-1] == "14:15 - 16:55":
                     late_penalty += 1
                 if slot[0] == "S":
                     saturday_penalty += 1
                 conflicts[course[0]][slot] = conflicts[course[0]].get(slot, 0) + 1
-                local_slots.add(slot)
                 unique_slots.add(slot)
-            fitness += 2*len(local_slots) / len(course[2]) # How many slots does a course cover relative to it section count
-            fitness += 3*len(local_days) / len(course[2])  # How many days does a course cover relative to it section count
+                fitness = fitness + 10*(len(days_set) + len(slots_set))/len(course[2])
+                if course[1] == "Lecture":
+                    fitness = fitness - abs(days_count.get(("M", "W"), 0)+days_count.get(("S", "M"), 0)+days_count.get(("S", "W"), 0) - days_count.get(("T", "R"),0))*5
+
+        # fitness /= len(schedule)
         conflict_sum = 0
         for conflict in conflicts.values():
             for value in conflict.values():
                 conflict_sum += value - 1
-        fitness -= (2*early_penalty + 1*late_penalty + 4*saturday_penalty + 0.5*sequenctial_pelanty+ 5*conflict_sum)
-        ratio = len(unique_slots) / len(lab_slots + lecture_slots)
-        fitness *= ratio
+        fitness = fitness + (-1*early_penalty - 2*late_penalty - 14*saturday_penalty  - 10*conflict_sum) # /10 - 1*sequenctial_pelanty
+        fitness *= (len(unique_slots)/(len(lecture_slots)+len(lab_slots)))
+        fitness /= len(schedule)
         return fitness, teacher_slots, [conflict_sum, sequenctial_pelanty, early_penalty, late_penalty, saturday_penalty]
--- final.py
+++ final.py
@@ -3,7 +3,7 @@ from Course import *
 from typing import *
 import time
 import random
-
+#  add comments for the whole file
 Population = list[Chromosome]
 
 
@@ -11,11 +11,10 @@ def generate_population(size: int) -> Population:
     population = []
     while True:
         schedule = Chromosome(None)
-        if schedule.fitness > 0:
+        if schedule.fitness:
             population.append(schedule)
         if len(population) == size:
             break
-        print(len(population), schedule.fitness)
     return population
 
 
@@ -26,9 +25,10 @@ def selection(population: Population, k: int) -> Population:
 
 
 def crossover(parent1: Chromosome, parent2: Chromosome) -> Population:
-    Parent1, Parent2 = copy.deepcopy(parent1), copy.deepcopy(parent2)
-    single_crossover_point = random.randint(1, len(parent1) - 1)
-    double_crossover_point = random.randint(1, len(parent1) - 1)
+    Parent1, Parent2 = copy.deepcopy(
+        parent1.courses), copy.deepcopy(parent2.courses)
+    single_crossover_point = random.randint(1, len(Parent1) - 1)
+    double_crossover_point = random.randint(1, len(Parent2) - 1)
     if single_crossover_point > double_crossover_point:
         single_crossover_point, double_crossover_point = double_crossover_point, single_crossover_point
     Child1 = Parent1[:single_crossover_point] + \
@@ -39,16 +39,15 @@ def crossover(parent1: Chromosome, parent2: Chromosome) -> Population:
         Parent2[double_crossover_point:]
     Child1 = Chromosome(Child1)
     Child2 = Chromosome(Child2)
-    return Child1, Child2
+    return Child1 if Child1.fitness else parent1, Child2 if Child2.fitness else parent2
 
 
 def mutation(chromosome: Chromosome, mutation_rate: float):
-    mutated_chromosome = copy.deepcopy(chromosome)
-    print(type(mutated_chromosome))
+    mutated_chromosome = copy.deepcopy(chromosome.courses)
     if random.random() < mutation_rate:
         for _ in range(10):  # 10 attempts to mutate
             course = random.randint(
-                0, len(mutated_chromosome) - 1)  # random course
+                10, len(mutated_chromosome) - 1)  # random course
             if mutated_chromosome[course][1] == "Lecture":
                 section = random.randint(
                     0, len(mutated_chromosome[course][2]) - 1)  # random section
@@ -60,43 +59,42 @@ def mutation(chromosome: Chromosome, mutation_rate: float):
                 mutated_chromosome[course][2][section][1] = random.choice(
                     lab_slots)  # mutate
             mutated_chromosome = Chromosome(mutated_chromosome)
-            if mutated_chromosome.fitness > 0:  # if mutation is valid
+            if mutated_chromosome.fitness:  # if mutation is valid
                 return mutated_chromosome
+            mutated_chromosome = mutated_chromosome.courses
     return chromosome
 
 
 def run_ga(initial_population: int, mutation_rate: float, generations: int, size: int) -> Population:
     population = generate_population(initial_population)
-    print(len(population))
     population.sort(key=lambda x: x.fitness, reverse=True)
-    print("Initial Fitness Range: ", round(
-        population[0].fitness, 2), round(population[0].fitness, 2))
-    print("Initial Conflicts: ", population[0].conflicts)
+    print("Initial Fitness Range: ", round(population[-1].fitness, 2), round(population[0].fitness, 2))
+    print("Initial Conflicts: ", population[0].conflict_sum)
     print("Initial Early Slots: ", population[0].early_penalty, "Late Slots: ",
           population[0].late_penalty, "Saturday Slots: ", population[0].saturday_penalty)
-    print("Initial Sequential Slots: ",
-          population[-1].sequintial_penalty, population[0].sequintial_penalty)
+    print("Initial Sequential Lectures: ", population[0].sequenctial_pelanty)
     start = time.time()
-    for _ in range(generations):
+    for j in range(generations):
         population = population[:size]
-        for _ in range(int(len(population) * 0.8)):
+        for i in range(int(len(population) * 0.8)):
             parents = selection(population, 2)
             offspring = crossover(parents[0], parents[1])
             offspring = [mutation(offspring[0], mutation_rate), mutation(
                 offspring[1], mutation_rate)]
             population.extend(offspring)
+        # print(f"Fitness {j}: ", ", ".join([str(round(x.fitness,3)) for x in population[:5]]))
         population.sort(key=lambda x: x.fitness, reverse=True)
-    print("Fitness Range: ", round(
-        population[0].fitness, 2), round(population[0].fitness, 2))
-    print("Conflicts: ", population[0].conflicts)
+    print("="*50)
+    print("Fitness {Range}: ", round(
+        population[-1].fitness, 2), round(population[0].fitness, 2))
+    print("Conflicts: ", population[0].conflict_sum)
     print("Early Slots: ", population[0].early_penalty, "Late Slots: ",
           population[0].late_penalty, "Saturday Slots: ", population[0].saturday_penalty)
-    print("Sequential Slots: ",
-          population[-1].sequintial_penalty, population[0].sequintial_penalty)
+    print("Sequential Lectures: ", population[0].sequenctial_pelanty)
     print("Time taken: ", time.time() - start)
     print("Top 5: ", ", ".join([str(x.fitness) for x in population[:5]]))
     print("Schedule: ")
     print_schedule(population[0])
 
 
-run_ga(200, 0.1, 1_000, 10)
+run_ga(initial_population=100, mutation_rate=0.3, generations=1000, size=100)
